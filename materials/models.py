d06a619f774f1fc03ea9b2122b243ce7143026e8
Elena_Syurtukova <evasineva@bk.ru>
2025-08-22 22:20:41 +0900

    models

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Сourse(models.Model):
+    title_course = models.CharField(max_length=50, verbose_name="название курса")
+    image_course = models.ImageField(
+        upload_to="materials/images/",
+        blank=True,
+        null=True,
+        verbose_name="превью курса",
+    )
+    description_course = models.TextField(blank=True, null=True, verbose_name="Описание")
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['title_course']
+
+    def __str__(self):
+        return self.title_course
+
+class Lesson(models.Model):
+    title_lesson = models.CharField(max_length=50, verbose_name="название урока")
+    image_lesson = models.ImageField(
+        upload_to="materials/images/",
+        blank=True,
+        null=True,
+        verbose_name="превью урока",
+    )
+    description_lesson = models.TextField(blank=True, null=True, verbose_name="Описание")
+    video_link = models.URLField(verbose_name="ссылка на видео урока")
+    course = models.ForeignKey(Сourse, on_delete=models.SET_NULL, verbose_name='курс', null=True, blank=True,
+                                 related_name='lessons')
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='владелец', null=True, blank=True,
+                              related_name='lessons')
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+        ordering = ['title_lesson', 'course', 'owner']
+
+    def __str__(self):
+        return self.title_lesson
