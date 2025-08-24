7b6facc091308f7a2a6ac94743eb95a929e36ba0
Elena_Syurtukova <evasineva@bk.ru>
2025-08-25 00:18:59 +0900

    fixture for payment, models for User, CourseSerializer

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from users.models import User, Payment
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email")
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "date_payment", "course", "lesson", "sum_payment", "method_payment")
+    list_filter = ("user", "date_payment", "course", "lesson", "sum_payment", "method_payment")
+    search_fields = ("user", "date_payment", "course", "lesson", "sum_payment", "method_payment")
--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from materials.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -33,3 +35,31 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    cash = "Наличные"
+    transfer = "Перевод на счет"
+    method_payment_choices = [(cash, "Наличные"), (transfer, "Перевод на счет")]
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             verbose_name="пользователь",
+                             null=True,
+                             blank=True
+                             )
+    date_payment = models.DateField(verbose_name="дата оплаты")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE,
+                               verbose_name="купленный курс",
+                               null=True,
+                               blank=True)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE,
+                               verbose_name="купленный урок",
+                               null=True,
+                               blank=True)
+    sum_payment = models.FloatField(verbose_name="сумма оплаты")
+    method_payment = models.CharField(
+        max_length=30,
+        choices=method_payment_choices,
+        default="Наличные",
+        verbose_name="способ оплаты",
+    )
