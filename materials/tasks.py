c5917725e9c0e3b1ca104d6fb44e5887af27aacf
Elena_Syurtukova <evasineva@bk.ru>
2025-09-20 19:59:54 +0900

    changes for HW33

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,10 +1,7 @@
-from datetime import datetime, timedelta
-
 from celery import shared_task
 from django.core.mail import send_mail
 
 from config.settings import EMAIL_HOST_USER
-from users.models import User
 
 
 @shared_task
@@ -17,12 +14,3 @@ def send_info_about_update_course(email):
 @shared_task
 def test_function():
     print("hellow")
-
-
-@shared_task
-def disactive_users():
-    users = User.objects.filter(is_active=True)
-    for user in users:
-        if datetime.now() - user.last_login > timedelta(days=30):
-            user.is_active = False
-            user.save()
